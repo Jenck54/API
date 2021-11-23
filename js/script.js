446f7b425e3cbbb43a99a453dc3c4c5475d13e7d
Jean Finck <jeanfinck29@gmail.com>
2021-11-23 13:50:50 +0100

    Jour2

--- js/script.js
+++ js/script.js
@@ -162,17 +162,43 @@ $(".moves").click(() => {
 })
 $(".pokemon").click(() => {
     $.get("https://pokeapi.co/api/v2/move/", (data) => {
-        $("#resultsBerries").html("")
-        $("#resultsContests").html("")
-        $("#resultsEncounters").html("")
-        $("#resultsEvolution").html("")
-        $("#resultsGames").html("")
-        $("#resultsItems").html("")
-        $("#resultsLocations").html("")
-        $("#resultsMachines").html("")
-        $("#resultsMachines").html("")
-        $("#resultsMoves").html("")
-        $("#resultsPokemon").html("")
+        $("#resultsBerries").html("").css("display","none")
+        for (let index = 0; index < data.results.length; index++) {
+            $("#resultsBerries").html($("#resultsBerries").html() + data.results[index].name + "<br>")
+        }
+        // $("#resultsBerries").html($("#resultsBerries").html() + "nombre :" + data.count)
+        $("#resultsBerries").html($("#resultsBerries").html() + "<button onclick=\"precedent('"+data.previous+"')\">Précedent</button>")
+        $("#resultsBerries").html($("#resultsBerries").html() + "<button onclick=\"suivant('"+data.next+"')\">Suivant</button>")
+    })
+})
+function suivant (url) {
+    let get_url = new url(url)
+    let search_url = get_url.searchParams
+    let url_limit = search_url.get('limit')
+        if (url_limit <20 ) {
+            search_url.set('limit', 20)
+            get_url.toString()
+        }
+    $.get(get_url.toString(), (data)=> {
+        $("#resultsBerries").html("").css("display","block")
+        for (let index = 0; index < data.results.length; index++) {
+            $("#resultsBerries").html($("#resultsBerries").html() + data.results[index].name + "<br>")
+        }
+        // $("#resultsBerries").html($("#resultsBerries").html() + "nombre :" + data.count)
+        $("#resultsBerries").html($("#resultsBerries").html() + "<button onclick=\"precedent('"+data.previous+"')\">Précedent</button>")
+        $("#resultsBerries").html($("#resultsBerries").html() + "<button onclick=\"suivant('"+data.next+"')\">Suivant</button>")
+    })
+}
+function precedent (url) {
+    let get_url = new url(url)
+    let search_url = get_url.searchParams
+    let url_limit = search_url.get('limit')
+        if (url_limit <20 ) {
+            search_url.set('limit', 20)
+            get_url.toString()
+        }
+    $.get(get_url.toString(), (data)=> {
+        $("#resultsBerries").html("").css("display","block")
         for (let index = 0; index < data.results.length; index++) {
             $("#resultsPokemon").html($("#resultsPokemon").html() + data.results[index].name + "<br>")
         }
