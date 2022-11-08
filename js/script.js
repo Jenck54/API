dad5a4fe0b4e17b7cba6718790f52cb4961b1814
Jean Finck <tidus202014@gmail.com>
2022-11-08 15:36:56 +0100

    API

--- js/script.js
+++ js/script.js
@@ -80,7 +80,7 @@ $(".machines").click(() => {
     $.get("https://pokeapi.co/api/v2/machine/", (data) => {
         $("#resultsBerries").html("").css("display","block")
         for (let index = 0; index < data.results.length; index++) {
-            $("#resultsBerries").html($("#resultsBerries").html() + "<div onclick =\"detailsMachines('"+ index.url + "')\">" + index.name + "</div>")
+            $("#resultsBerries").html($("#resultsBerries").html() + "<div onclick =\"detailsMachines('"+ data.results[index].url + "')\">" + index + "</div>")
         }
         // $("#resultsBerries").html($("#resultsBerries").html() + "nombre :" + data.count)
         $("#resultsBerries").html($("#resultsBerries").html() + "<button class=\"bouton\" onclick=\"precedent('"+data.previous+"')\">Précedent</button>")
@@ -120,7 +120,7 @@ function suivant (url) {
     $.get(get_url.toString(), (data)=> {
         $("#resultsBerries").html("").css("display","block")
         for (let index = 0; index < data.results.length; index++) {
-            $("#resultsBerries").html($("#resultsBerries").html() + data.results[index].name + "<br>")
+            $("#resultsBerries").html($("#resultsBerries").html() + "<div onclick =\"detailsBerries('"+ data.results[index].url + "')\">" + (data.results[index].name||(i+1)) + "</div>")
         }
         // $("#resultsBerries").html($("#resultsBerries").html() + "nombre :" + data.count)
         $("#resultsBerries").html($("#resultsBerries").html() + "<button class=\"bouton\" onclick=\"precedent('"+data.previous+"')\">Précedent</button>")
@@ -138,7 +138,7 @@ function precedent (url) {
     $.get(get_url.toString(), (data)=> {
         $("#resultsBerries").html("").css("display","block")
         for (let index = 0; index < data.results.length; index++) {
-            $("#resultsBerries").html($("#resultsBerries").html() + data.results[index].name + "<br>")
+            $("#resultsBerries").html($("#resultsBerries").html() + "<div onclick =\"detailsBerries('"+ data.results[index].url + "')\">" + (data.results[index].name||(i+1)) + "</div>")
         }
         // $("#resultsBerries").html($("#resultsBerries").html() + "nombre :" + data.count)
         $("#resultsBerries").html($("#resultsBerries").html() + "<button class=\"bouton\" onclick=\"precedent('"+data.previous+"')\">Précedent</button>")
@@ -146,7 +146,6 @@ function precedent (url) {
     })
 }
 function detailsBerries (url) {
-    console.log(url);
     $.get (url, (data)=> {
         $("#detailsBerries").html("").css("display","block")
         $("#detailsBerries").html($("#detailsBerries").html() + "Nom :" + data.name + "<br>")
@@ -162,5 +161,108 @@ function detailsBerries (url) {
         $("#detailsBerries").html($("#detailsBerries").html() + "Taille :" + data.size + "<br>")
         $("#detailsBerries").html($("#detailsBerries").html() + "Douceur :" + data.smoothness + "<br>")
         $("#detailsBerries").html($("#detailsBerries").html() + "Sécheresse du sol :" + data.soil_dryness + "<br>")
+    })
+}
+function detailsContests (url) {
+    $.get (url, (data)=> {
+        $("#detailsBerries").html("").css("display","block")
+        $("#detailsBerries").html($("#detailsBerries").html() + "Nom :" + data.name + "<br>")
+    })
+}
+function detailsEncounters (url) {
+    $.get (url, (data)=> {
+        $("#detailsBerries").html("").css("display","block")
+        $("#detailsBerries").html($("#detailsBerries").html() + "Nom :" + data.name + "<br>")
+    })
+}
+function detailsEvolution (url) {
+    $.get (url, (data)=> {
+        $("#detailsBerries").html("").css("display","block")
+        $("#detailsBerries").html($("#detailsBerries").html() + "Pokemon de base :" + data.chain.species.name + "<br>")
+        $("#detailsBerries").html($("#detailsBerries").html() + "1ere Evolution :" + data.chain.evolves_to[0].species.name + "<br>")
+        if (data.chain.evolves_to[0].evolves_to[0]!==undefined) {
+            $("#detailsBerries").html($("#detailsBerries").html() + "2e Evolution :" + data.chain.evolves_to[0].evolves_to[0].species.name + "<br>")
+        }
+    })
+}
+function detailsGames (url) {
+    $.get (url, (data)=> {
+        $("#detailsBerries").html("").css("display","block")
+        $("#detailsBerries").html($("#detailsBerries").html() + "Région principale :" + data.main_region.name + "<br>")
+        for (let index = 0; index < data.pokemon_species.length; index++) {
+            const element = data.pokemon_species[index];
+            if (element.name !==undefined) {
+                $("#detailsBerries").html($("#detailsBerries").html() + "Espèces de pokemon :" + element.name + "<br>")
+            }
+        }
+    })
+}
+function detailsItems (url) {
+    $.get (url, (data)=> {
+        $("#detailsBerries").html("").css("display","block")
+        $("#detailsBerries").html($("#detailsBerries").html() + "Nom :" + data.name + "<br>")
+        $("#detailsBerries").html($("#detailsBerries").html() + "Photo : <img class=\"image\" src='" + data.sprites.default + "'><br>")
+        for (let index = 0; index < data.attributes.length; index++) {
+            const element = data.attributes[index];
+            if (element.name !==undefined) {
+                $("#detailsBerries").html($("#detailsBerries").html() + "Attributs :" + element.name + "<br>")
+            }
+        }
+    })
+}
+function detailsLocations (url) {
+    $.get (url, (data)=> {
+        $("#detailsBerries").html("").css("display","block")
+        $("#detailsBerries").html($("#detailsBerries").html() + "Nom :" + data.name + "<br>")
+        for (let index = 0; index < data.areas.length; index++) {
+            const element = data.areas[index];
+            if (element.name !==undefined) {
+                $("#detailsBerries").html($("#detailsBerries").html() + "Zone :" + element.name + "<br>")
+            }
+        }
+        for (let index = 0; index < data.game_indices.length; index++) {
+            const element = data.game_indices[index];
+            if (element.generation.name !==undefined) {
+                $("#detailsBerries").html($("#detailsBerries").html() + "Generation :" + element.generation.name + "<br>")
+            }
+        }
+    })
+}
+function detailsMachines (url) {
+    $.get (url, (data)=> {
+        $("#detailsBerries").html("").css("display","block")
+        $("#detailsBerries").html($("#detailsBerries").html() + "Attaque :" + data.move.name + "<br>")
+        $("#detailsBerries").html($("#detailsBerries").html() + "Version de jeu :" + data.version_group.name + "<br>")
+    })
+}
+function detailsMoves (url) {
+    $.get (url, (data)=> {
+        $("#detailsBerries").html("").css("display","block")
+        $("#detailsBerries").html($("#detailsBerries").html() + "Nom :" + data.name + "<br>")
+        if (data.accuracy == null) {
+            data.accuracy = 0
+        }
+        $("#detailsBerries").html($("#detailsBerries").html() + "Précision :" + data.accuracy + "%" + "<br>")
+        $("#detailsBerries").html($("#detailsBerries").html() + "Type de dommage :" + data.damage_class.name + "<br>")
+        $("#detailsBerries").html($("#detailsBerries").html() + "Generation :" + data.generation.name + "<br>")
+        $("#detailsBerries").html($("#detailsBerries").html() + "Type d'attaque :" + data.type.name + "<br>")
+        if (data.power == null) {
+            data.power = 0
+        }
+        $("#detailsBerries").html($("#detailsBerries").html() + "Puissance :" + data.power + "<br>")
+        if (data.pp == null) {
+            data.pp = 0
+        }
+        $("#detailsBerries").html($("#detailsBerries").html() + "PP :" + data.pp + "<br>")
+        if (data.priority == null) {
+            data.priority = 0
+        }
+        $("#detailsBerries").html($("#detailsBerries").html() + "Priorité :" + data.priority + "<br>")
+        for (let index = 0; index < data.learned_by_pokemon.length; index++) {
+            const element = data.learned_by_pokemon[index];
+            if (element.name !== undefined) {
+                $("#detailsBerries").html($("#detailsBerries").html() + "Appris par :" + element.name + "<br>")
+            }
+        }
     })
 }
